17e2007eae812f647526e6e3fbe0c6827dfea806
miscellaneous-mice <prateeknarasimha@gmail.com>
2024-02-06 15:18:33 +0530

    initial commit

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,11 +1,19 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    vim.opt.termguicolors = true
-    require("bufferline").setup({})
-  end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		vim.opt.termguicolors = true
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				mode = "buffers",
+				style_preset = bufferline.style_preset.default,
+				separator_style = "slant",
+				diagnostics = "nvim_lsp",
+			},
+		})
+	end,
 }
